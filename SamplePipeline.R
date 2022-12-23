5829dedcbb0321aaccf81a1493ce8ef3178ddf0c
elisa <elibene@gmail.com>
2022-12-22 21:40:14 -0800

    Updated code to automatically download data from Figshare

--- SamplePipeline.R
+++ SamplePipeline.R
@@ -17,20 +17,69 @@ max_miss_filter <- 0.5
 # significance threshold
 alpha <- 0.05
 
-#### Load Data ----
+#### Define Helper Functions ----
 
-file_data <- data.makepath("ProstateCancer/EDRN/CORN-02-19ML+/CORN-02-19ML+ CDT_2019-12-23_MTfriendly.xlsx")
-file_clin <- data.makepath("ProstateCancer/EDRN/EDRN_ClinicalInfo_ForPublication.xlsx")
+# Downloads files from the web and verifies their checksum. 
+# If file already exists in working directoyy, will use local copy.
+load.web.file <- function(
+    url, md5sum, outfile, zipfile = F) {
+  # check if local file exists
+  if (file.exists(outfile)) {
+    # verify checksum
+    realsum <- tools::md5sum(outfile)[[1]]
+    if (realsum != md5sum) stop(sprintf("Local file %s has wrong checksum: %s", outfile, realsum))
+    # do not delete wrong file, it was already here before
+    
+  } else {
+    if(zipfile){
+      # download file
+      temp <- tempfile()
+      download.file(url,temp)
+      unzip(zipfile = temp, files = outfile, exdir = ".")
+    } else {
+      # download file
+      download.file(url, outfile)
+    }
+    # verify checksum
+    realsum <- tools::md5sum(outfile)[[1]]
+    if (realsum != md5sum) { 
+      # delete wrong file
+      unlink(outfile)
+      stop(sprintf("Remote file %s has wrong checksum: %s", url, realsum))
+    }
+  }
+}
+
+#### Download Data Files ----
+
+# file_clin <- data.makepath("ProstateCancer/EDRN/PublishedDataFiles/EDRN_ClinicalData.xlsx")
+# file_data <- data.makepath("ProstateCancer/EDRN/PublishedDataFiles/EDRN_MetabolomicsData.xlsx")
+
+# download metabolomics data from figshare
+# data files will be saved in the working directory
+file_data <- "EDRN_MetabolomicsData.xlsx"
+load.web.file(
+  url="https://figshare.com/ndownloader/files/38639168?private_link=90221d74cdf8ebad84c5",
+  md5sum = "97629808d6612cfe29cd84bd024ba96d",
+  outfile = file_data
+)
+
+# download clinical data from figshare
+# data files will be saved in the working directory
+file_clin <- "EDRN_ClinicalData.xlsx"
+load.web.file(
+  url="https://figshare.com/ndownloader/files/38628464?private_link=90221d74cdf8ebad84c5",
+  md5sum = "70324a7d65eb6a213eefdb4dca92d9ca",
+  outfile = file_clin
+)
+
+#### Lod Data ----
 
 D <- 
-  # header for html report
-  mt_reporting_heading(heading = "Data Loading", lvl = 1) %>%
-  # load metabolomics data
-  mt_load_xls(file=file_data, sheet="data", samples_in_rows = T, id_col = "SAMPLE_ID") %>% 
-  # load sample annotations
-  mt_anno_xls(file=file_data, sheet="sampleinfo", anno_type="samples", anno_id_col = "SAMPLE_ID") %>% 
-  # load metabolite annotations
-  mt_anno_xls(file=file_data, sheet="metinfo", anno_type="features", anno_id_col = "BIOCHEMICAL", data_id_col = "name") %>%
+  # load data
+  mt_load_xls(file=file_data, sheet="Data", samples_in_row=T, id_col="SAMPLE_ID") %>% 
+  mt_anno_xls(file=file_data, sheet="SampleAnnotations", anno_type="samples", anno_id_col ="SAMPLE_ID", data_id_col = "SAMPLE_ID") %>% 
+  mt_anno_xls(file=file_data, sheet="MetaboliteAnnotations", anno_type="features", anno_id_col="BIOCHEMICAL", data_id_col = "name") %>%
   # load clinical data
   mt_anno_xls(file=file_clin, sheet="ClinicalInfo", anno_type="samples", anno_id_col = "A1", data_id_col = "SAMPLE_ID") %>%
   # set variable to factor
@@ -93,7 +142,7 @@ D %<>%
   # plot dataset heatmap
   mt_plots_heatmap(scale_data = T, fontsize = 5, 
                    annotation_col = c("Diagnosis","Age"), 
-                   annotation_row = c("SUPER.PATHWAY"), 
+                   annotation_row = c("SUPER_PATHWAY"), 
                    show_colnames = F, show_rownames=F,
                    clustering_method = "ward.D2", cutree_rows = 5, cutree_cols = 5)
 
@@ -127,10 +176,15 @@ D %<>%
   mt_plots_stats_pathway_bar(stat_list = "Age", 
                              feat_filter = p.value<!!alpha,
                              y_scale = "count", assoc_sign_col = "statistic",
-                             group_col = "SUB.PATHWAY",
-                             color_col = "SUPER.PATHWAY") %>%
+                             group_col = "SUB_PATHWAY",
+                             color_col = "SUPER_PATHWAY") %>%
   # correct metabolites for Age using a linear model
   mt_pre_confounding_correction(formula = ~Age)
+
+#### Write Preprocessed Data and Annotations to File ----
+  
+D %>% mt_write_se_xls(file="EDRN_MetabolomicsData_Preprocessed.xlsx",
+                      sheet_names = c("PreprocessedData","MetaboliteAnnotations","ClinicalAnnotations"))
 
 #### Save html report ----
 
